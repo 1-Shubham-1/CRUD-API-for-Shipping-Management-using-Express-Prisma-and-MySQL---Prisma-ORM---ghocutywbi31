f4d52d9124fe083b92d4f8453578835a075df2aa
Shubham-60 <workshubham38@gmail.com>
2025-09-25 19:19:00 +0530

    changed error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,11 +5,15 @@ const verifySecret = (req, res, next) => {
   const secretKey = req.headers["shipping_secret_key"];
 
   if (!secretKey) {
-    return res.status(403).json({ error: "SHIPPING_SECRET_KEY is missing or invalid" });
+    return res.status(403).json({ 
+      "error": "SHIPPING_SECRET_KEY is missing or invalid"
+   });
   }
 
   if (secretKey !== process.env.SHIPPING_SECRET_KEY) {
-    return res.status(403).json({ error: "Failed to authenticate SHIPPING_SECRET_KEY" });
+    return res.status(403).json({ 
+      "error": "Failed to authenticate SHIPPING_SECRET_KEY"
+   });
   }
 
   next();
