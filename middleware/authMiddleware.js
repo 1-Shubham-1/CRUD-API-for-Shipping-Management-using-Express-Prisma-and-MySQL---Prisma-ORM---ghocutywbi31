b6d9498587faedbc951b3c0f2578871fafff9c01
Shubham-60 <workshubham38@gmail.com>
2025-09-30 09:33:22 +0530

    10

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const verifySecret = (req, res, next) => {
   const headers = (req && req.headers) ? req.headers : {};
-  const secretKey = headers["shipping_secret_key"];
+  const secretKey = req.headers["shipping_secret_key"] || req.headers["shipping-secret-key"];
 
   if (!secretKey || secretKey.trim() == "") {
     return res.status(403).json({ 
